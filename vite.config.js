f811b1212645855e491170927cde930e9fdcd67d
Admin <rashinkp001@gmail.com>
2025-01-07 20:33:41 +0530

    dsafsdf

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     host: "0.0.0.0",
   },
   build: {
-    outDir: "build", // Changed from 'dist' to 'build'
+    outDir: "dist", // Changed from 'dist' to 'build'
     sourcemap: true,
     chunkSizeWarningLimit: 1000,
   },
